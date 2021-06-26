36d631656970be237e44afa99e47a7a257bf187d
muhammad ameen <muhammadameenh1@gmail.com>
2021-06-26 19:22:32 +0500

    ecomerce app with slider

---
+++ lib/home.dart
@@ -0,0 +1,100 @@
+import 'package:flutter/material.dart';
+import 'package:carousel_slider/carousel_slider.dart';
+
+class Home extends StatefulWidget {
+  const Home({Key? key}) : super(key: key);
+
+  @override
+  _HomeState createState() => _HomeState();
+}
+
+class _HomeState extends State<Home> {
+  @override
+  Widget build(BuildContext context) {
+    return Scaffold(
+      appBar: AppBar(
+        title: Text("Ecom"),
+      ),
+      body: ListView(
+        children: [
+          CarouselSlider(
+            items: [
+              //1st Image of Slider
+              Container(
+                margin: EdgeInsets.all(6.0),
+                decoration: BoxDecoration(
+                  borderRadius: BorderRadius.circular(8.0),
+                  image: DecorationImage(
+                    image: AssetImage('assets/images/11.jpg'),
+                    fit: BoxFit.cover,
+                  ),
+                ),
+              ),
+
+              //2nd Image of Slider
+              Container(
+                margin: EdgeInsets.all(6.0),
+                decoration: BoxDecoration(
+                  borderRadius: BorderRadius.circular(8.0),
+                  image: DecorationImage(
+                    image: AssetImage('assets/images/22.jpg'),
+                    fit: BoxFit.cover,
+                  ),
+                ),
+              ),
+
+              //3rd Image of Slider
+              Container(
+                margin: EdgeInsets.all(6.0),
+                decoration: BoxDecoration(
+                  borderRadius: BorderRadius.circular(8.0),
+                  image: DecorationImage(
+                    image: AssetImage('assets/images/33.jpg'),
+                    fit: BoxFit.cover,
+                  ),
+                ),
+              ),
+
+              //4th Image of Slider
+              Container(
+                margin: EdgeInsets.all(6.0),
+                decoration: BoxDecoration(
+                  borderRadius: BorderRadius.circular(8.0),
+                  image: DecorationImage(
+                    image: AssetImage('assets/images/44.jpg'),
+                    fit: BoxFit.cover,
+                  ),
+                ),
+              ),
+
+              //5th Image of Slider
+              Container(
+                // child: Image.asset('assets/images/22.jpg'),
+                margin: EdgeInsets.all(6.0),
+                decoration: BoxDecoration(
+                  borderRadius: BorderRadius.circular(8.0),
+                  image: DecorationImage(
+                    image: AssetImage('assets/images/55.jpg'),
+                    fit: BoxFit.cover,
+                  ),
+                ),
+              ),
+            ],
+
+            //Slider Container properties
+            options: CarouselOptions(
+              height: 180.0,
+              enlargeCenterPage: true,
+              autoPlay: false,
+              aspectRatio: 16 / 9,
+              autoPlayCurve: Curves.fastOutSlowIn,
+              enableInfiniteScroll: true,
+              autoPlayAnimationDuration: Duration(milliseconds: 800),
+              viewportFraction: 0.8,
+            ),
+          ),
+        ],
+      ),
+    );
+  }
+}
